fa510393e7aaa0bb3016b9d09e03f14b043f906a
Jose Marquez <josemmarquezl@gmail.com>
2020-09-14 10:20:29 -0400

    configure nginx to be a layer 7 reverse-proxy server

--- node-hello/index.js
+++ node-hello/index.js
@@ -3,9 +3,13 @@ const express = require("express")
 const app = express()
 
 
+app.get("/node",(req,res)=>{
+  res.send("this is node of node")
+})
+
 app.get("/",(req,res)=>{
   res.send("hello from node")
 })
 
 app.listen(8080)
-console.log("node server running on port 8080")
+console.log("node server running")
